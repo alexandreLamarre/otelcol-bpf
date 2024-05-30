eeb84a61a84a99fde34e48958b4f13f3367e1e23
Alexandre Lamarre <alex7285@gmail.com>
2024-05-30 19:56:00 -0400

    Initial commit
    Signed-off-by: Alexandre Lamarre <alex7285@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexandreLamarre/otelbpf
+
+go 1.22.3
---
+++ receiver/bpfstack/doc.go
@@ -0,0 +1,4 @@
+//go:generate mdatagen metadata.yaml
+
+// Package bpfstack receives data from stack profiling generated from eBPF
+package bpfstack // import "go.opentelemetry.io/collector/receiver/bpfstack"

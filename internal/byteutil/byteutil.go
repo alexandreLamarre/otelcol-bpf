6cae190bb1b77f5102d6a9f0981982af870cd0c9
Alexandre Lamarre <alex7285@gmail.com>
2025-02-05 13:49:23 -0500

    (core) add byteutil to package
    Signed-off-by: Alexandre Lamarre <alex7285@gmail.com>

---
+++ internal/byteutil/byteutil.go
@@ -0,0 +1,42 @@
+package byteutil
+
+import (
+	"bytes"
+	"errors"
+)
+
+var (
+	ErrNoNullTerminator = errors.New("null terminator not found before array end")
+)
+
+func CCharSliceToStr(arr []int8) string {
+	byteSlice := make([]byte, len(arr))
+	for i, b := range arr {
+		byteSlice[i] = byte(b)
+	}
+	return string(byteSlice)
+
+}
+
+// ReadNullTerminatedString reads a null-terminated string into the provided array
+func ReadNullTerminatedString(buf *bytes.Buffer, arr []int8) error {
+	var last int8
+	for i := 0; i < len(arr); i++ {
+		b, err := buf.ReadByte()
+		if err != nil {
+			if err.Error() == "EOF" && last != 0 {
+				return ErrNoNullTerminator
+			} else if last == 0 {
+				return nil
+			}
+			return err
+		}
+		last = int8(b)
+		arr[i] = last
+
+		if b == 0 { // null terminator for C char *
+			return nil
+		}
+	}
+	return ErrNoNullTerminator
+}
---
+++ internal/byteutil/byteutil_test.go
@@ -0,0 +1,39 @@
+package byteutil_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/alexandreLamarre/otelcol-bpf/internal/byteutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCCharSliceToStr(t *testing.T) {
+	byteArray := []int8{104, 101, 108, 108, 111, 0}
+
+	ret := byteutil.CCharSliceToStr(byteArray)
+	require.Equal(t, ret, "hello\x00")
+}
+
+func TestReadNullTerminatedString(t *testing.T) {
+	// fits within buffer and has a null terminator
+	b := []byte{'a', '\x00', 'b'}
+	arr := make([]int8, 2)
+
+	require.NoError(t, byteutil.ReadNullTerminatedString(bytes.NewBuffer(b), arr))
+	require.Equal(t, arr, []int8{97, 0})
+
+	// fits, but no null terminatior
+	b2 := []byte{'a'}
+	arr2 := make([]int8, 2)
+	err := byteutil.ReadNullTerminatedString(bytes.NewBuffer(b2), arr2)
+	require.Equal(t, err, byteutil.ErrNoNullTerminator)
+
+	// doesn't fit, no null terminator
+
+	b3 := []byte{'a', 'b', 'c'}
+	arr3 := make([]int8, 3)
+	err = byteutil.ReadNullTerminatedString(bytes.NewBuffer(b3), arr3)
+	require.Equal(t, err, byteutil.ErrNoNullTerminator)
+
+}

fe6b478def8034c1e0409c72cb07a29368e0bdae
Alexandre Lamarre <alex7285@gmail.com>
2025-02-07 13:34:37 -0500

    remove bpfproc receiver
    Signed-off-by: Alexandre Lamarre <alex7285@gmail.com>

--- receiver/bpfproc/factory.go
+++
@@ -1,7 +0,0 @@
-package bpfproc
-
-import "go.opentelemetry.io/collector/receiver"
-
-func NewFactory() receiver.Factory {
-	return nil
-}
--- receiver/bpfproc/tools.go
+++
@@ -1,7 +0,0 @@
-//go:build tools
-
-package bpfproc
-
-import (
-	_ "github.com/cilium/ebpf/cmd/bpf2go"
-)

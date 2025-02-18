93cfd12c2029becb96f32277590ac23f3b07883b
Alexandre Lamarre <alex7285@gmail.com>
2025-02-18 10:49:01 -0500

    add otel metrics sdk/ integration test to tcpbpf
    Signed-off-by: Alexandre Lamarre <alex7285@gmail.com>

--- bpf/go.mod
+++ bpf/go.mod
@@ -9,18 +9,36 @@ require (
 	github.com/jedib0t/go-pretty/v6 v6.6.5
 	github.com/spf13/cobra v1.8.1
 	github.com/stretchr/testify v1.10.0
+	go.opentelemetry.io/collector/component v0.120.0
+	go.opentelemetry.io/otel v1.34.0
+	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v1.34.0
+	go.opentelemetry.io/otel/metric v1.34.0
+	go.uber.org/goleak v1.3.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/collector/pdata v1.26.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.34.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.34.0 // indirect
+	go.opentelemetry.io/otel/trace v1.34.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
+	google.golang.org/grpc v1.70.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ bpf/tcp/metrics.go
@@ -0,0 +1,125 @@
+package tcp
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/alexandreLamarre/otelcol-bpf/bpf/pkg/byteutil"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+)
+
+type TCPMetrics struct {
+	ctx               context.Context
+	BpfTCPConnlatency metric.Int64Histogram
+	BpfTCPRx          metric.Int64Gauge
+	BpfTCPTx          metric.Int64Gauge
+}
+
+func NewTcpMetrics(ctx context.Context, meter metric.Meter) (*TCPMetrics, error) {
+	t := &TCPMetrics{
+		ctx: ctx,
+	}
+	var errs error
+	var err error
+	t.BpfTCPConnlatency, err = meter.Int64Histogram(
+		"bpf.tcp.connlatency",
+		metric.WithDescription("Histogram of TCP connection latency"),
+		metric.WithUnit("ns"),
+		metric.WithExplicitBucketBoundaries(0, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000),
+	)
+	errs = errors.Join(errs, err)
+	t.BpfTCPRx, err = meter.Int64Gauge(
+		"bpf.tcp.rx",
+		metric.WithDescription("TCP received bytes"),
+		metric.WithUnit("bytes"),
+	)
+	errs = errors.Join(errs, err)
+	t.BpfTCPTx, err = meter.Int64Gauge(
+		"bpf.tcp.tx",
+		metric.WithDescription("TCP transmitted bytes"),
+		metric.WithUnit("bytes"),
+	)
+	errs = errors.Join(errs, err)
+	return t, errs
+}
+
+func (t *TCPMetrics) ConnLatCallback(event TcpconnLatEvent) {
+	name := byteutil.CCharSliceToStr(event.Comm[:])
+	isIpv4 := byteutil.EmptyIpv6(byteutil.Ipv6Str(event.SaddrV6))
+	var saddr, daddr string
+	if isIpv4 {
+		saddr = byteutil.Ipv4Str(event.SaddrV4)
+		daddr = byteutil.Ipv4Str(event.DaddrV4)
+	} else {
+		saddr = byteutil.Ipv6Str(event.SaddrV6)
+		daddr = byteutil.Ipv6Str(event.DaddrV6)
+	}
+	saddr += fmt.Sprintf(":%d", event.Lport)
+	daddr += fmt.Sprintf(":%d", event.Dport)
+	attrs := metric.WithAttributes(attribute.KeyValue{
+		Key:   attribute.Key("saddr"),
+		Value: attribute.StringValue(saddr),
+	},
+		attribute.KeyValue{
+			Key:   attribute.Key("daddr"),
+			Value: attribute.StringValue(daddr),
+		},
+		attribute.KeyValue{
+			Key:   attribute.Key("comm"),
+			Value: attribute.StringValue(name),
+		},
+		attribute.KeyValue{
+			Key:   attribute.Key("tgid"),
+			Value: attribute.Int64Value(int64(event.Tgid)),
+		},
+		attribute.KeyValue{
+			Key:   attribute.Key("af"),
+			Value: attribute.Int64Value(int64(event.Af)),
+		},
+	)
+	t.BpfTCPConnlatency.Record(t.ctx, int64(event.DeltaUs), attrs)
+}
+
+func (t *TCPMetrics) ConnStatsCallback(key TcpconnlatTrafficKey, val TcpconnlatTrafficValue) {
+	isIpv4 := byteutil.EmptyIpv6(byteutil.Ipv6Str(key.SaddrV6))
+	name := byteutil.CCharSliceToStr(key.Name[:])
+	var saddr, daddr string
+	if isIpv4 {
+		saddr = byteutil.Ipv4Str(key.SaddrV4)
+		daddr = byteutil.Ipv4Str(key.DaddrV4)
+	} else {
+		saddr = byteutil.Ipv6Str(key.SaddrV6)
+		daddr = byteutil.Ipv6Str(key.DaddrV6)
+	}
+	saddr += fmt.Sprintf(":%d", key.Lport)
+	daddr += fmt.Sprintf(":%d", key.Dport)
+	attrs := metric.WithAttributes(attribute.KeyValue{
+		Key:   attribute.Key("saddr"),
+		Value: attribute.StringValue(saddr),
+	},
+		attribute.KeyValue{
+			Key:   attribute.Key("daddr"),
+			Value: attribute.StringValue(daddr),
+		},
+		attribute.KeyValue{
+			Key:   attribute.Key("comm"),
+			Value: attribute.StringValue(name),
+		},
+		attribute.KeyValue{
+			Key:   attribute.Key("pid"),
+			Value: attribute.Int64Value(int64(key.Pid)),
+		},
+	)
+
+	if rx := val.Rx; rx > 0 {
+		t.BpfTCPRx.Record(t.ctx, int64(val.Rx), attrs)
+
+	}
+	if tx := val.Tx; tx > 0 {
+
+		t.BpfTCPTx.Record(t.ctx, int64(val.Tx), attrs)
+	}
+
+}
